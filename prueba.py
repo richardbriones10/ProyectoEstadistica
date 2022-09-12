10cb3f716cf29e1976b2865c1a954a018e4e3a2c
Richard <richardgomezbriones@gmail.com>
2022-09-12 16:25:01 -0600

    graficas con matplotpy

--- prueba.py
+++ prueba.py
@@ -2,16 +2,14 @@
 import tkinter
 from tkinter import *
 import tkinter as tk
+import numpy as np
+import matplotlib.pyplot as plt
 
 import matplotlib
 
 matplotlib.use('TkAgg')
 
 # Variables
-sumtotal = 0
-sumhi = 0
-sumfi = 0
-sumaP = 0
 fi = []
 Hi = []
 hi = []
@@ -30,12 +28,11 @@ canvas1.create_window(200, 140, window=entry1)
 
 
 def getSquareRoot():
-    x1 = int(entry1.get())
+    x1 = entry1.get()
 
-    lista.append(x1)
-
-    # for i in range(20):
-    #     lista.append(i)
+    # lista.append(x1)
+    for i in range(20):
+        lista.append(20-i)
 
     label1 = tk.Label(root, text=x1)
     canvas1.create_window(200, 230, window=label1)
@@ -131,10 +128,6 @@ class Table:
                 self.e.insert(END, lst[i][j])
 
 
-class Graph:
-    sumtotal1 = 1
-
-
 # CREA LA VENTANA DE TABLA DE FRECUENCIAS
 root = Tk()
 root.title("TABLA DE FRECUENCIAS")
@@ -142,36 +135,50 @@ t = Table(root)
 root.mainloop()  # CIERRA LA VENTANA DE TABLA DE FRECUENCIAS
 
 # ABRE LA VENTANA DE MEDIA,MEDIANA Y MODA
-root = tkinter.Tk()
-root.title("GRAFICAS")
-# g = Graph(root)
+# root = tkinter.Tk()
+# root.title("GRAFICAS")
 
-c_width = 1080
-c_height = 720
-c = tkinter.Canvas(root, width=c_width, height=c_height, bg='white')
-c.pack()
+# c_width = 720
+# c_height = 480
+# c = tkinter.Canvas(root, width=c_width, height=c_height, bg='white')
+# c.pack()
+#
+# # the variables below size the bar graph
+# # experiment with them to fit your needs
+# # highest y = max_data_value * y_stretch
+# y_stretch = 15
+# # gap between lower canvas edge and x axis
+# y_gap = 20
+# # stretch enough to get all data items in
+# x_stretch = 10
+# x_width = 20
+# # gap between left canvas edge and y axis
+# x_gap = 20
+#
+# for x, y in enumerate(lista):
+#     # calculate reactangle coordinates (integers) for each bar
+#     x0 = x * x_stretch + x * x_width + x_gap
+#     y0 = c_height - int(y * y_stretch + y_gap)
+#     x1 = x * x_stretch + x * x_width + x_width + x_gap
+#     y1 = c_height - y_gap
+#     # draw the bar
+#     c.create_rectangle(x0, y0, x1, y1, fill="red")
+#     # put the y value above each bar
+#     c.create_text(x0 + 2, y0, anchor=tkinter.SW, text=str(y))
 
-# the variables below size the bar graph
-# experiment with them to fit your needs
-# highest y = max_data_value * y_stretch
-y_stretch = 15
-# gap between lower canvas edge and x axis
-y_gap = 20
-# stretch enough to get all data items in
-x_stretch = 10
-x_width = 20
-# gap between left canvas edge and y axis
-x_gap = 20
+data = lista
+newdata = np.squeeze(data)  # Shape is now: (10, 80)
+plt.bar(newdata,newdata)  # plotting by columns
+plt.bar(newdata,newdata)  # plotting by columns
+plt.xlabel("Indices")
+plt.ylabel("Frecuencias")
+plt.show()
 
-for x, y in enumerate(lista):
-    # calculate reactangle coordinates (integers) for each bar
-    x0 = x * x_stretch + x * x_width + x_gap
-    y0 = c_height - int(y * y_stretch + y_gap)
-    x1 = x * x_stretch + x * x_width + x_width + x_gap
-    y1 = c_height - y_gap
-    # draw the bar
-    c.create_rectangle(x0, y0, x1, y1, fill="red")
-    # put the y value above each bar
-    c.create_text(x0 + 2, y0, anchor=tkinter.SW, text=str(y))
+# root.mainloop() #CIERRE DE LAS GRAFICAS
 
-root.mainloop()
+# MEDIA ARITMETICA
+suma_total_frecuencia = 0
+for i in lista:
+    suma_total_frecuencia = suma_total_frecuencia + int(i)
+media = suma_total_frecuencia / len(lista)
+print(f"Media:{media}")
